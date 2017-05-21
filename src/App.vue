c9aac5029fe2f0d15147121741c312cde7dbfbde
weiqinl <772863869@qq.com>
2017-05-21 13:51:07 +0800

    按需引入，设置对比结果

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,10 @@
     <el-button>默认按钮</el-button>
     <el-button type="primary">主要按钮</el-button>
     <el-button type="text">文字按钮</el-button>
-     <el-input-number v-model="num1" @change="handleChange" :min="1" :max="10"></el-input-number>
-
+    <el-input-number v-model="num1" @change="handleChange" :min="1" :max="10"></el-input-number>
+    <br />
+    <el-input placeholder="elinput输入框">a</el-input>
+    <input placeholder="input输入框">b</input>
   </div>
 </template>
 
