adf12fd04f4966e240138c165211a4f666eb4cc9
刘伟勤 <wbliuwq@yonyou.com>
2017-05-27 15:26:17 +0800

    想要default-active 效果实现，菜单title，不要用span标签包围

--- src/components/nav/siderbar/sidebar.vue
+++ src/components/nav/siderbar/sidebar.vue
@@ -2,16 +2,19 @@
   <div class="wq-sidebar">
     <el-row class="tac">
       <el-col :span="24">
-        <el-menu theme="dark" class="menu-ul":default-active="$route.path" :unique-opened=false router @open="handleOpen" @close="handleClose" v-for="(item, index) in menuItem" :key="index">
-          <el-menu-item class="menu-li"  v-if="typeof item.child === 'undefined'" :index="item.path"><i class="icon" :class="item.icon"></i><span v-text="item.title" class="text"></span>
+        <el-menu theme="dark" class="menu-ul" :default-active="$route.path" :unique-opened=true router @open="handleOpen" @close="handleClose" v-for="(item, index) in menuItem" :key="index">
+          <el-menu-item class="menu-li"  v-if="typeof item.child === 'undefined'" :index="item.path">
+          <i class="icon" :class="item.icon"></i>
+            {{ item.title }}
           </el-menu-item>
           <el-submenu v-else :index="item.path" >
             <template slot="title">
               <i class="icon" :class="item.icon"></i>
-              <span v-text="item.title" class="text"></span>
+              {{ item.title }}
             </template>
             <el-menu-item class="menu-li" v-for="(child_item, child_index) in item.child" :index="child_item.path" :key="child_index"  >
-            <span v-text="child_item.title" class="text"></span>
+            <i class="icon" :class="child_item.icon"></i>
+            {{ child_item.title }}
             </el-menu-item>
           </el-submenu>
         </el-menu>
@@ -26,12 +29,14 @@ return {
     menuItem: [{
       title: '首页',
       path: '/home',
+      icon: 'el-icon-menu'
     },{
       title:'表格管理',
       path: '/tables',
+      icon: 'el-icon-setting',
       child: [{
         title: '基础表格',
-        path: '/tables/basic'
+        path: '/tables/basic',
       },{
         title: '固定标头表格',
         path: '/tables/fixedheader'
